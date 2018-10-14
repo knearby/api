298db94a453420237fb22d75c2f97450a9556ebd
zephinzer <dev@joeir.net>
2018-10-14 15:56:25 +0800

    initial commit with randomPlace

---
+++ functions/index.js
@@ -0,0 +1,116 @@
+const functions = require('firebase-functions');
+
+const geohasher = require('./utils/geohasher');
+const requester = require('./utils/requester');
+
+const API_KEY = requester.getEnvValue('GOOGLE_PLACES_API_KEY');
+const REQ_HOST = 'maps.googleapis.com';
+const REQ_METHOD = 'GET';
+
+exports.randomPlace = functions.https.onRequest((request, response) => {
+  const {body, headers, method, query} = request;
+  let geohash, latitude, longitude, radius;
+  switch (method) {
+    case 'GET':
+      response.status(400);
+      response.header('Content-Type', 'application/json');
+
+      if (!query.text) {
+        response.send(JSON.stringify({
+          message: 'missing \'text\' query parameter',
+        }));
+        return;
+      } else if (!query.lat) {
+        response.send(JSON.stringify({
+          message: 'missing \'lat\' query parameter',
+        }));
+        return;
+      } else if (!query.lon) {
+        response.send(JSON.stringify({
+          message: 'missing \'lon\' query parameter',
+        }));
+        return;
+      } else if (!query.rad) {
+        response.send(JSON.stringify({
+          message: 'missing \'rad\' query parameter',
+        }));
+        return;
+      }
+
+      try {
+        const {lat, lon} = query;
+        const floatLat = parseFloat(lat);
+        const floatLon = parseFloat(lon);
+        if (isNaN(floatLat)) {
+          throw new Error('Latitude parsing failed - is it a float?');
+        } else if (isNaN(floatLon)) {
+          throw new Error('Longitude parsing failed - is it a float?');
+        }
+        geohash = geohasher.encode({
+          latitude: floatLat,
+          longitude: floatLon,
+        });
+        latitude = floatLat;
+        longitude = floatLon;
+      } catch (ex) {
+        response.send(JSON.stringify({
+          message: 'geohashing failed',
+          data: ex,
+        }));
+        return;
+      }
+
+      try {
+        const {rad} = query;
+        const intRad = parseInt(rad);
+        if (isNaN(intRad)) {
+          throw new Error('Radius parsing failed - is it an integer?');
+        }
+        radius = intRad;
+      } catch (ex) {
+        response.send(JSON.stringify({
+          message: 'radius failed',
+          data: ex,
+        }));
+        return;
+      }
+
+      
+      requester.getData({
+        id: 'get-places',
+        hostname: REQ_HOST,
+        method: REQ_METHOD,
+        platform: 'google',
+        useCache: true,
+        uriPath:
+          '/maps/api/place/nearbysearch/json?' +
+          [
+            `key=${API_KEY}`,
+            `keyword=${encodeURIComponent(query.text)}`,
+            `location=${latitude},${longitude}`,
+            `radius=${radius}`
+          ].join('&')
+      }).then((data) => {
+        response.status(200);
+        const randomPlace = data.results[Math.floor(Math.random() * (data.results.length - 1))];
+        console.info(randomPlace);
+        response.send(
+          JSON.stringify({
+            id: randomPlace.id,
+            name: randomPlace.name,
+            rating: randomPlace.rating ? randomPlace.rating : null,
+            tags: randomPlace.types ? randomPlace.types : [],
+            address: randomPlace.vicinity ? randomPlace.vicinity : null,
+          })
+        );
+        return;
+      }).catch((err) => {
+        console.error(err);
+        response.status(500);
+        response.send(JSON.stringify(err));
+      });
+      break;
+    default:
+      response.send(JSON.stringify('nah, can\'t ' + method + ' this shiz.'));
+  }
+});
---
+++ functions/utils/requester.js
@@ -0,0 +1,162 @@
+const crypto = require('crypto');
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+
+module.exports = {
+  getData,
+  getEnvValue,
+};
+
+function createCacheHash({
+  method,
+  hostname,
+  uriPath,
+}) {
+  const unhashedData = method + hostname + uriPath;
+  return crypto.createHash('md5').update(unhashedData).digest('hex');
+}
+
+function createCachePath({
+  id,
+  platform,
+  cacheHash,
+}) {
+  return path.join(__dirname, `../cache/${platform}-${id}-${cacheHash}.json`);
+}
+
+function getData({
+  hostname,
+  id = 'unknown',
+  method = 'GET',
+  platform = '',
+  uriPath = '/',
+  useCache = false,
+}) {
+  
+  return new Promise((resolve, reject) => {
+    const cacheOptions = {
+      hostname, id, method, platform, uriPath,
+    };
+    const cachedData = getCache(cacheOptions);
+
+    if (useCache && cachedData !== null ) {
+      resolve(cachedData);
+    } else {
+      getResponse({
+        hostname,
+        uriPath,
+        method,
+      }, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          setCache(cacheOptions, data);
+          resolve(data);
+        }
+      });
+    }
+  });
+}
+
+function getResponse({
+  hostname,
+  method,
+  uriPath,
+}, callback) {
+  const requestOptions = {
+    hostname,
+    port: 443,
+    path: uriPath,
+    method,
+  };
+
+  let responseBody = '';
+
+  const request = https.request(
+    requestOptions,
+    (res) => {
+      res.on('data', (data) => responseBody += data.toString());
+      res.on('end', () => {
+        let parsedData;
+        try {
+          parsedData = JSON.parse(responseBody);
+        } catch (ex) {
+          return callback(ex);
+        }
+        return callback(null, parsedData)
+      });
+    }
+  );
+  request.on('error', (error) => callback(error));
+  request.end();
+}
+
+function getEnvValue(keyString) {
+  return (process.env[keyString])
+    ? process.env[keyString]
+    : fs
+    .readFileSync(path.join(__dirname, '../.env'))
+    .toString()
+    .split('\n')
+    .filter((v) => v.indexOf(keyString) === 0)[0]
+    .split('=')[1];
+}
+
+/**
+ * 
+ * @param {Object} options
+ * @param {String} options.hostname
+ * @param {String} options.id
+ * @param {String} options.method
+ * @param {String} options.platform
+ * @param {String} options.uriPath
+ */
+function getCache({
+  hostname,
+  id,
+  method,
+  platform,
+  uriPath,
+}) {
+  const cacheHash = createCacheHash({method, hostname, uriPath});
+  const pathToData = createCachePath({id, platform, cacheHash});
+
+  return (fs.existsSync(pathToData))
+    ? JSON.parse(fs.readFileSync(pathToData).toString())
+    : null;
+}
+
+/**
+ * 
+ * @param {Object} options
+ * @param {String} options.hostname
+ * @param {String} options.id
+ * @param {String} options.method
+ * @param {String} options.platform
+ * @param {String} options.uriPath
+ * @param {Any} data
+ */
+function setCache(
+  {
+    hostname,
+    id,
+    method,
+    platform,
+    uriPath,
+  },
+  data
+) {
+  const cacheHash = createCacheHash({method, hostname, uriPath});
+  const pathToData = createCachePath({id, platform, cacheHash});
+
+  // setting a try/catch block because in production this will 100% fail
+  try {
+    fs.writeFileSync(
+      pathToData,
+      JSON.stringify(data, null, 2)
+    );
+  } catch (ex) {
+    console.error(ex);
+  }
+}
