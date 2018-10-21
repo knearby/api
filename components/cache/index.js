4df4a2644ddfb5f0ceb9d2b8baef79377b305127
zephinzer <dev@joeir.net>
2018-10-21 16:23:15 +0800

    added cache poc

---
+++ components/cache/index.js
@@ -0,0 +1,148 @@
+const firebaseAdmin = require('firebase-admin');
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(require('./credentials.json')),
+  databaseURL: 'https://the-karma-app.firebaseio.com/',
+});
+const db = firebaseAdmin.database();
+
+module.exports = {
+  getPlaceAutocompleteCache,
+  getPlaceInfoCache,
+  getPlacesCache,
+  setPlaceAutocompleteCache,
+  setPlaceInfoCache,
+  setPlacesCache,
+};
+
+/**
+ * @param {String} searchTerm
+ * @return {Promise<Object>}
+ */
+function getPlaceAutocompleteCache(searchTerm) {
+  if (!searchTerm || typeof searchTerm !== 'string') {
+    throw new Error('searchTerm was not defined or not a string');
+  }
+  return new Promise((resolve, reject) => {
+    try {
+      const ref = db.ref(`cache/${setPlaceAutocompleteCache.key}/${searchTerm}`);
+      ref.once('value').then((snapshot) => resolve(Object.assign({}, {path: ref.path.pieces_.join('/')}, snapshot.val())));
+    } catch (ex) {
+      reject(ex);
+    }
+  });
+}
+
+/**
+ * @param {String} searchTerm
+ * @param {Object} results
+ * @return {String}
+ */
+function setPlaceAutocompleteCache(searchTerm, results) {
+  if (!searchTerm || typeof searchTerm !== 'string') {
+    throw new Error('searchTerm was not defined or not a string');
+  } else if (!results || typeof results !== 'object') {
+    throw new Error('results was not defined or not an object')
+  }
+  const ref = db.ref(`cache/${setPlaceAutocompleteCache.key}/${searchTerm}`);
+  ref.set(Object.assign({}, results, createBootstrapFields()));
+  return ref.path;
+}
+
+setPlaceAutocompleteCache.key = 'gmaps/place/autocomplete';
+setPlaceAutocompleteCache.description = 'each key in here represents an autocomplete attempt containing an array of results returned by the Google Maps API';
+// // enable for debug 
+// setPlaceAutocompleteCache('__README', {description: setPlaceAutocompleteCache.description});
+// getPlaceAutocompleteCache('__README').then(console.info);
+
+
+/**
+ * @param {String} searchTerm
+ * @return {Promise<Object>}
+ */
+function getPlaceInfoCache(placeId) {
+  if (!placeId || typeof placeId !== 'string') {
+    throw new Error('placeId was not defined or not a string');
+  }
+  return new Promise((resolve, reject) => {
+    try {
+      const ref = db.ref(`cache/${setPlaceInfoCache.key}/${placeId}`);
+      ref.once('value').then((snapshot) => resolve(Object.assign({}, {path: ref.path.pieces_.join('/')}, snapshot.val())));
+    } catch (ex) {
+      reject(ex);
+    }
+  });
+}
+
+/**
+ * @param {String} placeId placeId returned by Google Maps API
+ * @param {Object} results place object returned by Google Maps API
+ * @return {String}
+ */
+function setPlaceInfoCache(placeId, results) {
+  if (!placeId || typeof placeId !== 'string') {
+    throw new Error('placeId was not defined or not a string');
+  } else if (!results || typeof results !== 'object') {
+    throw new Error('results was not defined or not an object')
+  }
+  const ref = db.ref(`cache/${setPlaceInfoCache.key}/${placeId}`);
+  ref.set(
+    Object.assign({}, results, createBootstrapFields()));
+  return ref.path;
+}
+
+setPlaceInfoCache.key = 'gmaps/place/info';
+setPlaceInfoCache.description = 'each key in here represents a placeId returned by the Google Maps API';
+// // enable for debug
+// setPlaceInfoCache('__README', {description: setPlaceInfoCache.description});
+// getPlaceInfoCache('__README').then(console.info);
+
+/**
+ * @param {String} searchTerm
+ * @return {Promise<Object>}
+ */
+function getPlacesCache(geohash) {
+  if (!geohash || typeof geohash !== 'string') {
+    throw new Error('geohash was not defined or not a string');
+  }
+  return new Promise((resolve, reject) => {
+    try {
+      const ref = db.ref(`cache/${setPlacesCache.key}/${geohash}`);
+      ref.once('value').then((snapshot) => resolve(Object.assign({}, {path: ref.path.pieces_.join('/')}, snapshot.val())));
+    } catch (ex) {
+      reject(ex);
+    }
+  });
+}
+
+/**
+ * @param {String} geohash 
+ * @param {Object} results 
+ * @return {Object}
+ */
+function setPlacesCache(geohash, results) {
+  if (!geohash || typeof geohash !== 'string') {
+    throw new Error('geohash was not defined or not a string');
+  } else if (!results || typeof results !== 'object') {
+    throw new Error('results was not defined or not an object')
+  }
+  const ref = db.ref(`cache/${setPlacesCache.key}/${geohash}`);
+  ref.set(
+    Object.assign({}, results, createBootstrapFields()));
+  return ref.path;
+}
+
+setPlacesCache.key = 'gmaps/place/s';
+setPlacesCache.description = 'each key in here represents a geohash and contains a list of results returned by the Google Maps API';
+// // enable for debug
+// setPlacesCache('__README', {description: setPlacesCache.description});
+// getPlacesCache('__README').then(console.info);
+
+/**
+ * @return {Object}
+ */
+function createBootstrapFields() {
+  return {
+    timestamp: (new Date()).getTime(),
+    environment: process.env.NODE_ENV || 'development',
+  };
+}
